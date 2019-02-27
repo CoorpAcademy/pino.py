16ceb61dd33f755de2565880dc48120a652bf9c5
Adriean Khisbe <adriean.khisbe@live.fr>
2019-02-27 17:18:08 +0100

    Add possibility to change stream of logger :ocean:

--- pino/core.py
+++ pino/core.py
@@ -1,4 +1,6 @@
 import json
+import sys
+import os
 
 LEVELS = {
     "error": 40,
@@ -9,17 +11,19 @@ LEVELS = {
 
 class PinoLogger:
 
-    def __init__(self, bindings=None, level="info"):
+    def __init__(self, bindings=None, level="info", stream=sys.stdout):
         self._logger_level = LEVELS[level]
         self._logger_metas = bindings or {}
         self._is_logging = True
+        self._stream = stream
 
     def _log(self, metas, message, level = "info"):
         # for simple of api, expect a single message
         if self._is_logging and LEVELS[level] >= self._logger_level:
             real_message = message or metas
             message_metas = metas if message else {}
-            print(json.dumps({"message": real_message, "level": level, **self._logger_metas, **message_metas}))
+            self._stream.write(json.dumps({"message": real_message, "level": level, **self._logger_metas, **message_metas}))
+            self._stream.write(os.linesep)
 
     def info(self, metas, message=None):
         self._log(metas, message, level = "info")
