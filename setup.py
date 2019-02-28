a447f8a61070087dcad86b9eb0c83182c06bf04b
Adriean Khisbe <adriean.khisbe@live.fr>
2019-02-28 10:03:55 +0100

    Make a 0.2 release with .child and time value :rocket:

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="pino",
-    version="0.1.0",
+    version="0.2.0",
     author="Adrien Becchis @Coorpacademy",
     author_email="adriean.khisbe@live.fr",
     description="Python json logger",
