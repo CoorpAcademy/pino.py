27b7115ea71e8db213c7a84bed5e4e59493698d8
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-09-22 22:36:37 +0200

    Rework the pino dynamic method to avoid no-member lint error :eyes:

--- pino/core.py
+++ pino/core.py
@@ -24,34 +24,44 @@ def get_logger(stream, level, metas={}):
         has_meta = isinstance(args[0], dict)
         message_metas = args[0] if has_meta else None
         complete_metas = (merge_dicts(metas, message_metas)
-                 if message_metas else metas)
-        message = args[1] if has_meta else args[0]
-        stream.write(json.dumps({
-                "level": level,
-                "time": int(1000* datetime.now().timestamp()),
-                # §todo: add host and other metas.
-                "message": message,
-                **complete_metas}))
+                          if message_metas else metas)
+        message = args[1] if has_meta else args[0]  # !TODO: handle formating!
+        json_log = {
+            "level": level,
+            "time": int(1000* datetime.now().timestamp()),
+            # §todo: add host and other metas.
+            "message": message,
+            **complete_metas
+        }
+        stream.write(json.dumps(json_log))
         stream.write(os.linesep)
         stream.flush()
     log.__name__ = level
     return log
 
-def noop(*args):
-    pass
+class DummyLogger:
+    def fatal(self, *args):
+        pass
+    def error(self, *args):
+        pass
+    def warn(self, *args):
+        pass
+    def info(self, *args):
+        pass
+    def debug(self, *args):
+        pass
 
-class PinoLogger:
+class PinoLogger(DummyLogger):
+    __slots__ = ["_config"]
     def __init__(self, bindings=None, level="info", stream=sys.stdout, enabled=True, parent=None):
         logging_level = LEVEL_BY_NAME.get(level)  # ! TODO: support LoggingLevel or Code?
         self._config = PinoConfig(logging_level, stream, bindings, enabled, parent)
         for level in LEVEL_NAMES:
-            logging_method = (get_logger(stream, level, bindings)
-              if enabled and LEVEL_BY_NAME.get(level).level >= logging_level.level
-              else noop)
-            setattr(self, level, logging_method)
+            if enabled and LEVEL_BY_NAME.get(level).level >= logging_level.level:
+                logging_method = get_logger(stream, level, bindings)
+                setattr(self, level, logging_method)
 
     def child(self, metas):
-        # §TODO: handle level? (nd other options)
         merged_bindings = merge_dicts(self._config.binding, metas)
         return PinoLogger(
             bindings=merged_bindings,
