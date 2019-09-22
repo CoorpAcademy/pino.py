779ba6ecb910d08b81d0fcddee6581ba6b3c18b1
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-09-22 11:29:22 +0200

    Major refactor to avoid level check at logging time :hammer_and_wrench:

--- pino/core.py
+++ pino/core.py
@@ -4,47 +4,49 @@ import os
 from datetime import datetime
 from .utils import merge_dicts
 
-LEVELS = {
-    "fatal": 50,
-    "error": 40,
-    "warn": 30,
+LEVELS_NAME_TO_LEVEL = {
+    "debug": 10,
     "info": 20,
-    "debug": 10
+    "warn": 30,
+    "error": 40,
+    "fatal": 50
 }
+LEVEL_NAMES = LEVELS_NAME_TO_LEVEL.keys()
+
+
+def get_logger(self, level):
+    def log(*args):
+        has_meta = isinstance(args[0], dict)
+        message_metas = args[0] if has_meta else None
+        metas = (merge_dicts(self._logger_metas, message_metas)
+                 if message_metas else self._logger_metas)
+        message = args[1] if has_meta else args[0]
+        self._stream.write(json.dumps({
+                "level": level,
+                "time": int(1000* datetime.now().timestamp()),
+                # §todo: add host and other metas.
+                "message": message,
+                **metas}))
+        self._stream.write(os.linesep)
+        self._stream.flush()
+    log.__name__ = level
+    return log
+
+def noop(*args):
+    pass
 
 class PinoLogger:
-
     def __init__(self, bindings=None, level="info", stream=sys.stdout, enabled=True):
         self._level = level
-        self._logger_level = LEVELS[level]
+        self._logger_level = LEVELS_NAME_TO_LEVEL[level]
         self._logger_metas = bindings or {}
         self._is_logging = enabled
         self._stream = stream
-
-    def _log(self, metas, message, level = "info"):
-        # for simple of api, expect a single message
-        if self._is_logging and LEVELS[level] >= self._logger_level:
-            real_message = message or metas
-            message_metas = metas if message else {}
-            self._stream.write(json.dumps({
-                 "level": level,
-                 "time": int(1000* datetime.now().timestamp()),
-                 # §todo: add host and other metas.
-                 "message": real_message,
-                 **merge_dicts(self._logger_metas, message_metas)}))
-            self._stream.write(os.linesep)
-            self._stream.flush()
-
-    def fatal(self, metas, message=None):
-        self._log(metas, message, level="fatal")
-    def info(self, metas, message=None):
-        self._log(metas, message, level="info")
-    def error(self, metas, message=None):
-        self._log(metas, message, level="error")
-    def warn(self, metas, message=None):
-        self._log(metas, message, level="warn")
-    def debug(self, metas, message=None):
-        self._log(metas, message, level="debug")
+        for level in LEVEL_NAMES:
+            logging_method = (get_logger(self, level)
+              if enabled and LEVELS_NAME_TO_LEVEL[level] >= self._logger_level
+              else noop)
+            setattr(self, level, logging_method)
 
     def child(self, metas):
         # §TODO: handle level?
