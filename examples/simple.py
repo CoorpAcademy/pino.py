e5028d180ee08d8bd431946a69d05f3195496630
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-09-23 21:09:03 +0200

    Set up millidiff option to logger :stopwatch:

--- examples/simple.py
+++ examples/simple.py
@@ -3,14 +3,17 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 ## ^^ Do not use statement up when installing library ^^
-
+from time import sleep
 from pino import pino
 
 logger = pino(bindings={"apptype": "prototype"})
 
 logger.info("toto")
+sleep(0.1)
 logger.debug("tutu")
+sleep(0.012)
 logger.warn({"err_code": 42}, "tata")
+sleep(0.010)
 logger.error("tete")
 
 child_logger = logger.child({"apptype": "mega-prototype"})
--- pino/core.py
+++ pino/core.py
@@ -6,7 +6,7 @@ from collections import namedtuple
 from .utils import merge_dicts
 
 LoggingLevel = namedtuple('LoggingLevel', ['name', 'level'])
-PinoConfig = namedtuple('PinoConfig', ['level', 'stream', 'bindings', 'enabled', 'parent'])
+PinoConfig = namedtuple('PinoConfig', ['level', 'stream', 'bindings', 'enabled', 'millidiff', 'parent'])
 
 LEVELS = [
    LoggingLevel("debug", 10),
@@ -23,19 +23,26 @@ def get_logger(self):
     metas = self._config.bindings
     level = self._config.level.name
     stream = self._config.stream
+    should_millidiff = self._config.millidiff
+
     def log(*args):
         has_meta = isinstance(args[0], dict)
         message_metas = args[0] if has_meta else None
         complete_metas = (merge_dicts(metas, message_metas)
                           if message_metas else metas)
         message = args[1] if has_meta else args[0]  # !TODO: handle formating!
+        now = int(1000* datetime.now().timestamp())
         json_log = {
             "level": level,
-            "time": int(1000* datetime.now().timestamp()),
+            "time": now,
             # §todo: add host and other metas.
             "message": message,
             **complete_metas
         }
+        if should_millidiff:
+            delta = (now - self._last_timestamp) if self._last_timestamp else 0
+            json_log["millidiff"] = delta
+            self._last_timestamp = now
         stream.write(json.dumps(json_log))
         stream.write(os.linesep)
         stream.flush()
@@ -55,10 +62,11 @@ class DummyLogger:
         pass
 
 class PinoLogger(DummyLogger):
-    __slots__ = ["_config"]
-    def __init__(self, bindings=None, level="info", stream=sys.stdout, enabled=True, parent=None):
+    __slots__ = ["_config", "_last_timestamp"]
+    def __init__(self, bindings=None, level="info", stream=sys.stdout, enabled=True, parent=None, millidiff=True):
         logging_level = LEVEL_BY_NAME.get(level)  # ! TODO: support LoggingLevel or Code?
-        self._config = PinoConfig(logging_level, stream, bindings, enabled, parent)
+        self._config = PinoConfig(logging_level, stream, bindings, enabled, millidiff, parent)
+        self._last_timestamp = None
         for level in LEVEL_NAMES:
             if enabled and LEVEL_BY_NAME.get(level).level >= logging_level.level:
                 logging_method = get_logger(self)
@@ -66,10 +74,13 @@ class PinoLogger(DummyLogger):
 
     def child(self, metas):
         merged_bindings = merge_dicts(self._config.bindings, metas)
-        return PinoLogger(
+        child_logger = PinoLogger(
             bindings=merged_bindings,
             level=self._config.level.name,
             enabled=self._config.enabled,
+            millidiff=self._config.millidiff,
             stream=self._config.stream,
             parent=self
         )
+        child_logger._last_timestamp = self._last_timestamp
+        return child_logger
