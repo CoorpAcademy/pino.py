325840b2f37b83eebd50862cac6d7c96d28583e6
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-09-23 21:17:20 +0200

    Some more level refactor :hammer_and_wrench:

--- pino/core.py
+++ pino/core.py
@@ -8,15 +8,22 @@ from .utils import merge_dicts
 LoggingLevel = namedtuple('LoggingLevel', ['name', 'level'])
 PinoConfig = namedtuple('PinoConfig', ['level', 'stream', 'bindings', 'enabled', 'millidiff', 'parent'])
 
-LEVELS = [
-   LoggingLevel("debug", 10),
-   LoggingLevel("info", 20),
-   LoggingLevel("warn", 30),
-   LoggingLevel("error", 40),
-   LoggingLevel("fatal", 50)
-]
+DEBUG = LoggingLevel("debug", 10)
+INFO = LoggingLevel("info", 20)
+WARN = LoggingLevel("warn", 30)
+ERROR = LoggingLevel("error", 40)
+FATAL = LoggingLevel("fatal", 50)
+
+LEVELS = [DEBUG, INFO, WARN, ERROR, FATAL]
 LEVEL_NAMES = [level.name for level in LEVELS]
 LEVEL_BY_NAME = {level.name: level for level in LEVELS}
+LEVEL_BY_CODE = {level.level: level for level in LEVELS}
+def get_level(level_name_or_code):
+    if isinstance(level_name_or_code, LoggingLevel):
+        return level_name_or_code
+    return (LEVEL_BY_CODE.get(level_name_or_code)
+            if isinstance(level_name_or_code, int)
+            else LEVEL_BY_NAME.get(level_name_or_code))
 
 
 def get_logger(self):
@@ -64,13 +71,13 @@ class DummyLogger:
 class PinoLogger(DummyLogger):
     __slots__ = ["_config", "_last_timestamp"]
     def __init__(self, bindings=None, level="info", stream=sys.stdout, enabled=True, parent=None, millidiff=True):
-        logging_level = LEVEL_BY_NAME.get(level)  # ! TODO: support LoggingLevel or Code?
+        logging_level = get_level(level)  # ! TODO: support LoggingLevel or Code?
         self._config = PinoConfig(logging_level, stream, bindings, enabled, millidiff, parent)
         self._last_timestamp = None
-        for level in LEVEL_NAMES:
-            if enabled and LEVEL_BY_NAME.get(level).level >= logging_level.level:
+        for level in LEVELS:
+            if enabled and level.level >= logging_level.level:
                 logging_method = get_logger(self)
-                setattr(self, level, logging_method)
+                setattr(self, level.name, logging_method)
 
     def child(self, metas):
         merged_bindings = merge_dicts(self._config.bindings, metas)
